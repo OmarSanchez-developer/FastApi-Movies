364f26a61723ecf7a873e421c186052e3050d77e
Omar Sanchez <ing_omarglez@hotmail.com>
2023-10-19 22:12:58 -0600

    Metodos PUT, GET, DELETE, POST
    Categories, movies, CRUD.

---
+++ main.py
@@ -0,0 +1,76 @@
+from fastapi import FastAPI, Body
+from fastapi.responses import HTMLResponse
+app = FastAPI()
+app.title = "Mi aplicación con FastApi"
+app.version = "0.0.1"
+
+movies = [
+    {
+        "id": 1,
+        "title": "Avatar",
+        "overview": "En un momento planeta llamado Pandora viven los Nav",
+        "year": "2009",
+        "rating": 7.8,
+        "category": "Accion"
+    },
+     {
+        "id": 2,
+        "title": "Avatar",
+        "overview": "En un momento planeta llamado Pandora viven los Nav",
+        "year": "2009",
+        "rating": 7.8,
+        "category": "Accion"
+    }
+]
+
+@app.get('/', tags=['home'])
+def message():
+    return HTMLResponse('<h1>Hello world</h1>')
+
+@app.get('/movies', tags=['movies'])
+def get_movies():
+    return movies
+
+@app.get('/movies/{id}')
+def get_movie(id: int):
+    for item in movies:
+        if item["id"] == id:
+            return item
+    return []
+
+@app.get('/movies/', tags=['movies'])
+def get_movies_by_category(category: str, year: int):
+    return [item for item in movies if item['category'] == category]
+
+@app.post('/movies', tags=['movies'])
+def create_movie(id: int = Body(), title: str = Body(), overview: str = Body(), year: int = Body(), raiting: float = Body(), category: str = Body()):
+    movies.append({
+        "id": id,
+        "title": title,
+        "overview": overview,
+        "year": raiting,
+        "category": category
+    })
+    return movies
+
+@app.put('/movies/{id}', tags=['movies'])
+def update_movie(id: int, title: str = Body(), overview:str = Body(), year:int = Body(), raiting:float = Body(), category:str = Body()):
+     for item in movies:
+       if item["id"] == id:
+            item['title'] = title,
+            item['overview'] = overview,
+            item['year'] = year,
+            item['raiting'] = raiting,
+            item['category'] = category
+            return movies
+       
+@app.delete('/movies/{id}', tags=['movies'])
+def delete_movie(id: int):
+    for item in movies:
+         if item["id"] == id:
+            movies.remove(item)
+            return movies
+       
+
+
+     
